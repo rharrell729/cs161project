d832300620919b0fdd41e87888ef1531af0ef4be
Wenyi Jones <wenyijones@Wenyis-iMac.local>
2018-03-14 12:00:51 -0400

    make CLCSFast compilable

--- CLCSFast.py
+++ CLCSFast.py
@@ -3,18 +3,29 @@ import numpy as np
 
 arr = np.zeros((2048, 2048), dtype=int)
 
+
+# p0 is from (0,0) to (m,n)
+# pm is from (m, 0) to (2m, n) 
+
+#need to initialize storage for p.
+p=[None]*20
+
 #recursive method
-def FINDSHORTESTPATHS(A,B, p,l,u)
-	if u−l ≤ 1 return
-	mid ← (l +u)/2
-	p[mid] ← SINGLESHORTESTPATH(A,B,mid, p[l], p[u])
+def FINDSHORTESTPATHS(A,B, p,l,u):
+    if (u-l) <= 1:
+        return 
+	mid = (l+u)/2
+	p[mid] = SINGLESHORTESTPATH(A,B,mid, p[l], p[u])
 #store mid for use as bounds (l and u) in next recursive step. How to store it?? 	
 	FINDSHORTESTPATHS(A,B, p,l,mid)
 	FINDSHORTESTPATHS(A,B, p,mid,u)
 
 
-#LCS needs to be modified to only check the relevant paths: the nested for loops should only check items within the bounds of the shortest paths explored prior (stored in the singleshortestpath method). We might need to update the arguments in 'range' each time we call singleshortest
-def SINGLESHORTESTPATH(A,B,mid,p[l],p[u]):
+#LCS needs to be modified to only check the relevant paths: the nested for loops 
+#should only check items within the bounds of the shortest paths explored prior 
+#(stored in the singleshortestpath method). We might need to update the arguments 
+#in 'range' each time we call singleshortest
+def SINGLESHORTESTPATH(A,B,mid,pl,pu):
 	m = len(A)
 	n = len(B)
 
